6395ef6e47baad7be3297f54fd7cc302e05bcfcf
RidoyChandraShil <88985410+RidoyChandraShil@users.noreply.github.com>
2021-10-02 05:17:09 +0600

    Update image cropping.m

--- image cropping.m
+++ image cropping.m
@@ -5,9 +5,9 @@ rx=1;
 ry=1;
 newim=subim(f,m,n,rx,ry);
 function s = subim(f, m, n, rx, ry)
-s = zeros(m,n);
-rxend = rx+m-1;
-ryend = ry+n-1;
-s = f(rx:rxend,ry:ryend);
-imshow(s);
+  s = zeros(m,n);
+  rxend = rx+m-1;
+  ryend = ry+n-1;
+  s = f(rx:rxend,ry:ryend);
+  imshow(s);
 end
