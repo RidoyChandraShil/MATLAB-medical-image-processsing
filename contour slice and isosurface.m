01e868e9483159a68d14aec4022b755958fee4de
RidoyChandraShil <88985410+RidoyChandraShil@users.noreply.github.com>
2021-10-02 04:05:05 +0600

    Update contour slice and isosurface.m

--- contour slice and isosurface.m
+++ contour slice and isosurface.m
@@ -8,11 +8,14 @@ xslice = [-1.2,0.8,2];
 lvls = -0.2:0.01:0.4;
 
 contourslice(X,Y,Z,V,xslice,[],[],lvls)
+title('Contourslice','FontSize',20)
 colorbar
 view(3)
 grid on
+
 %isosurface
 clf;
-isosurface(x, y, z, v, 1e-5);
-isosurface(x, y, z, v, -1e-4);
+isosurface(X,Y,Z,V, 1e-5);
+isosurface(X,Y,Z,V, -1e-4);
 axis([-3 3 -3 3 -3 3]);
+title('Isosurface','FontSize',20)
