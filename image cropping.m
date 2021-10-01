be2faca254c25f8cf14f4149a2e140e3621425dc
RidoyChandraShil <88985410+RidoyChandraShil@users.noreply.github.com>
2021-10-02 02:48:03 +0600

    Update image cropping.m

--- image cropping.m
+++ image cropping.m
@@ -1,6 +1,13 @@
+f=imread('pout.tif');
+m=200;
+n=150;
+rx=1;
+ry=1;
+newim=subim(f,m,n,rx,ry);
 function s = subim(f, m, n, rx, ry)
 s = zeros(m,n);
 rxend = rx+m-1;
 ryend = ry+n-1;
 s = f(rx:rxend,ry:ryend);
 imshow(s);
+end
