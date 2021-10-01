3d5f9c0f003ba3e982bbdaa07f21d9e4b4042a84
RidoyChandraShil <88985410+RidoyChandraShil@users.noreply.github.com>
2021-10-02 03:15:06 +0600

    Update image multiplication.m

--- image multiplication.m
+++ image multiplication.m
@@ -24,3 +24,31 @@ pmax = max(p(:));
 pmin = min(p(:));
 pn = mat2gray(p);
 end
+
+%% with image
+f1=imread('shadow.tif');
+g1=imread('pout.tif');
+newf1=subim(f1,223,240,1,1);
+newg1=subim(g1,223,240,1,1);
+result=improd(newf1,newg1);
+function s = subim(f, m, n, rx, ry)
+s = zeros(m,n);
+rxend = rx+m-1;
+ryend = ry+n-1;
+s = f(rx:rxend,ry:ryend);
+imshow(s);
+end
+function [p, pmax, pmin, pn] = improd(f, g)
+fd = double(f);
+gd = double(g);
+p = fd.*gd;
+pmax = max(p(:));
+pmin = min(p(:));
+pn = mat2gray(p);
+figure(1)
+imshow(f,[])
+figure(2)
+imshow(g,[])
+figure(3)
+imshow(p,[])
+end
