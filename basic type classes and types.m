32182c0c0e37e9c242742d8d71bd38fcd3f183c1
RidoyChandraShil <88985410+RidoyChandraShil@users.noreply.github.com>
2021-10-02 03:13:56 +0600

    Update basic type classes and types.m

--- basic type classes and types.m
+++ basic type classes and types.m
@@ -6,3 +6,22 @@ d= im2double(b)
 e= mat2gray(b)
 f= im2single(b)
 g= im2bw(b)
+
+%% with image
+f=imread('pout.tif');
+g=im2uint8(f);
+h=uint16(f)
+g1= im2double(f);
+g2= mat2gray(f);
+g3= mat2gray(f,[75 250]);
+figure(1)
+subplot 151
+imshow(f)
+subplot 152
+imshow(g)
+subplot 153
+imshow(g1)
+subplot 154
+imshow(g2)
+subplot 155
+imshow(g3)
